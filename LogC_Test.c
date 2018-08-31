d065c00b42578270a7d2be0b6e173c7d2cda74aa
XXXBold <30773753+XXXBold@users.noreply.github.com>
2018-08-31 15:08:53 +0000

    Update LogC_Test.c

--- LogC_Test.c
+++ LogC_Test.c
@@ -1,3 +1,7 @@
+
+
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
@@ -20,14 +24,14 @@
     #undef LOGC_TEST_THREADS
     #undef WIN32_LEAN_AND_MEAN
     #define LOGC_TEST_THREADS 1
-    typedef unsigned int tThreadReturn;
+    typedef unsigned int TThreadReturn;
   #elif defined(__unix__)
     #include <unistd.h> /* For determining the current POSIX-Version */
     #if defined(_POSIX_VERSION) && (_POSIX_VERSION >= 200112L) /* POSIX IEEE 1003.1-2001 or newer */
       #include <pthread.h>
       #undef LOGC_TEST_THREADS
       #define LOGC_TEST_THREADS 2
-      typedef void* tThreadReturn;
+      typedef void* TThreadReturn;
     #else
       #error POSIX Version too old, must be POSIX IEEE 1003.1-2001 or newer for pthreads.
     #endif /* POSIX Version */
@@ -43,8 +47,8 @@
                                   "----------")
 
 #ifdef LOGC_FEATURE_ENABLE_LOGFILE
-  #define LOGFILE_PATH "F:\\Projects\\Libraries\\LibLog\\LibLogTest\\Test1.log"
-  #define LOGFILE_PATH2 "F:\\Projects\\Libraries\\LibLog\\LibLogTest\\Test2.log"
+  #define LOGFILE_PATH "./Test1.log"
+  #define LOGFILE_PATH2 "./Test2.log"
 #endif /* LOGC_FEATURE_ENABLE_LOGFILE */
 
 #ifdef LOGC_FEATURE_ENABLE_LOG_STORAGE
@@ -84,7 +88,6 @@ int main(void)
 #ifdef LOGFILE_PATH
   TagLogFile tagLogFile;
   tagLogFile.pcFilePath=LOGFILE_PATH;
-  tagLogFile.szMaxFileQueueSize=20;
 #endif /* LOGFILE_PATH */
   LOGC_TEST_TRACE("Creating Log-Object...");
   if(!(ptagLog_m=ptagLogC_New_g(LOGC_ALL,
@@ -116,7 +119,6 @@ int main(void)
   LOGC_TEST_TRACE("Adding some entries...");
   LOGC_TEST_TEXT(ptagLog_m,LOGC_WARNING,   "Test_Warning_123456789abcdefghijklmnopqrstuvwxyz\n");
   LOGC_TEST_TEXT(ptagLog_m,LOGC_ERROR,     "Test_Error_123456789abcdefghijklmnopqrstuvwxyz");
-  LOGC_TEST_TEXT(ptagLog_m,LOGC_FATAL,     "Test_Fatal_123456789abcdefghijklmnopqrstuvwxyz");
 
   LOGC_TEST_TRACE("Changing Log options to LOGC_PREFIX_FILEINFO...");
   if(iLogC_SetPrefixOptions_g(ptagLog_m,
@@ -125,37 +127,87 @@ int main(void)
     LOGC_TEST_TRACE("iLogC_SetPrefixOptions_g() failed");
     LOGTEST_EXIT_FAILURE(ptagLog_m);
   }
-
   LOGC_TEST_TRACE("Adding some more entries...");
   vLogTestFunc1_g();
   vLogTestFunc2_g();
-  vLogTestFunc3_g();
 
-  LOGC_TEST_TRACE("Changing Prefix options to LOGC_PREFIX_TIMESTAMP...");
+  LOGC_TEST_TRACE("Changing Prefix options to LOGC_OPTION_PREFIX_TIMESTAMP_DATE...");
   if(iLogC_SetPrefixOptions_g(ptagLog_m,
-                              LOGC_OPTION_PREFIX_TIMESTAMP))
+                              LOGC_OPTION_PREFIX_TIMESTAMP_DATE))
   {
     LOGC_TEST_TRACE("iLogC_SetPrefixOptions_g() failed");
     LOGTEST_EXIT_FAILURE(ptagLog_m);
   }
-
   LOGC_TEST_TRACE("Adding some more entries...");
   LOGC_TEST_TEXT(ptagLog_m,LOGC_WARNING,   "Test_Warning_123456789abcdefghijklmnopqrstuvwxyz\n");
   LOGC_TEST_TEXT(ptagLog_m,LOGC_ERROR,     "Test_Error_123456789abcdefghijklmnopqrstuvwxyz");
-  LOGC_TEST_TEXT(ptagLog_m,LOGC_FATAL,     "Test_Fatal_123456789abcdefghijklmnopqrstuvwxyz");
 
-  LOGC_TEST_TRACE("Changing Prefix options to (LOGC_PREFIX_FILEINFO|LOGC_PREFIX_TIMESTAMP)...");
+  LOGC_TEST_TRACE("Changing Prefix options to LOGC_OPTION_PREFIX_TIMESTAMP_TIME...");
   if(iLogC_SetPrefixOptions_g(ptagLog_m,
-                           (LOGC_OPTION_PREFIX_FILEINFO|LOGC_OPTION_PREFIX_TIMESTAMP)))
+                              LOGC_OPTION_PREFIX_TIMESTAMP_TIME))
   {
     LOGC_TEST_TRACE("iLogC_SetPrefixOptions_g() failed");
     LOGTEST_EXIT_FAILURE(ptagLog_m);
   }
-
   LOGC_TEST_TRACE("Adding some more entries...");
   vLogTestFunc1_g();
   vLogTestFunc2_g();
-  vLogTestFunc3_g();
+
+  LOGC_TEST_TRACE("Changing Prefix options to LOGC_OPTION_PREFIX_TIMESTAMP_TIME_MILLISECS...");
+  if(iLogC_SetPrefixOptions_g(ptagLog_m,
+                              LOGC_OPTION_PREFIX_TIMESTAMP_TIME_MILLISECS))
+  {
+    LOGC_TEST_TRACE("iLogC_SetPrefixOptions_g() failed");
+    LOGTEST_EXIT_FAILURE(ptagLog_m);
+  }
+  LOGC_TEST_TRACE("Adding some more entries...");
+  vLogTestFunc1_g();
+  vLogTestFunc2_g();
+
+  LOGC_TEST_TRACE("Changing Prefix options to (LOGC_OPTION_PREFIX_TIMESTAMP_DATE|LOGC_OPTION_PREFIX_TIMESTAMP_TIME)...");
+  if(iLogC_SetPrefixOptions_g(ptagLog_m,
+                              (LOGC_OPTION_PREFIX_TIMESTAMP_DATE|LOGC_OPTION_PREFIX_TIMESTAMP_TIME)))
+  {
+    LOGC_TEST_TRACE("iLogC_SetPrefixOptions_g() failed");
+    LOGTEST_EXIT_FAILURE(ptagLog_m);
+  }
+  LOGC_TEST_TRACE("Adding some more entries...");
+  vLogTestFunc1_g();
+  vLogTestFunc2_g();
+
+  LOGC_TEST_TRACE("Changing Prefix options to (LOGC_OPTION_TIMESTAMP_TIME|LOGC_OPTION_PREFIX_TIMESTAMP_TIME_MILLISECS)...");
+  if(iLogC_SetPrefixOptions_g(ptagLog_m,
+                              (LOGC_OPTION_PREFIX_TIMESTAMP_TIME|LOGC_OPTION_PREFIX_TIMESTAMP_TIME_MILLISECS)))
+  {
+    LOGC_TEST_TRACE("iLogC_SetPrefixOptions_g() failed");
+    LOGTEST_EXIT_FAILURE(ptagLog_m);
+  }
+  LOGC_TEST_TRACE("Adding some more entries...");
+  vLogTestFunc1_g();
+  vLogTestFunc2_g();
+
+  LOGC_TEST_TRACE("Changing Prefix options to (LOGC_OPTION_PREFIX_TIMESTAMP_DATE|LOGC_OPTION_PREFIX_TIMESTAMP_TIME_MILLISECS)...");
+  if(iLogC_SetPrefixOptions_g(ptagLog_m,
+                              (LOGC_OPTION_PREFIX_TIMESTAMP_DATE|LOGC_OPTION_PREFIX_TIMESTAMP_TIME_MILLISECS)))
+  {
+    LOGC_TEST_TRACE("iLogC_SetPrefixOptions_g() failed");
+    LOGTEST_EXIT_FAILURE(ptagLog_m);
+  }
+  LOGC_TEST_TRACE("Adding some more entries...");
+  vLogTestFunc1_g();
+  vLogTestFunc2_g();
+
+  LOGC_TEST_TRACE("Changing Prefix options to (LOGC_OPTION_PREFIX_TIMESTAMP_DATE|LOGC_OPTION_PREFIX_TIMESTAMP_TIME|LOGC_OPTION_PREFIX_TIMESTAMP_TIME_MILLISECS)...");
+  if(iLogC_SetPrefixOptions_g(ptagLog_m,
+                              (LOGC_OPTION_PREFIX_TIMESTAMP_DATE|LOGC_OPTION_PREFIX_TIMESTAMP_TIME|LOGC_OPTION_PREFIX_TIMESTAMP_TIME_MILLISECS)))
+  {
+    LOGC_TEST_TRACE("iLogC_SetPrefixOptions_g() failed");
+    LOGTEST_EXIT_FAILURE(ptagLog_m);
+  }
+  LOGC_TEST_TRACE("Adding some more entries...");
+  vLogTestFunc1_g();
+  vLogTestFunc2_g();
+
 
 #ifdef LOGFILE_PATH
   if(iLogTest_File_g(ptagLog_m))
@@ -173,14 +225,14 @@ int main(void)
   }
 #endif /* LOG_MAX_STORAGE_COUNT */
 
-  LOGC_TEST_TRACE("Changing Prefix options to LOGC_PREFIX_TIMESTAMP...");
+#ifdef LOGC_TEST_THREADS
+  LOGC_TEST_TRACE("Changing Prefix options to (LOGC_OPTION_PREFIX_TIMESTAMP_TIME|LOGC_OPTION_PREFIX_TIMESTAMP_TIME_MILLISECS)...");
   if(iLogC_SetPrefixOptions_g(ptagLog_m,
-                           LOGC_OPTION_PREFIX_TIMESTAMP))
+                              (LOGC_OPTION_PREFIX_TIMESTAMP_TIME|LOGC_OPTION_PREFIX_TIMESTAMP_TIME_MILLISECS)))
   {
     LOGC_TEST_TRACE("iLogC_SetPrefixOptions_g() failed");
     LOGTEST_EXIT_FAILURE(ptagLog_m);
   }
-#ifdef LOGC_TEST_THREADS
   if(iLogTest_Threads_m(ptagLog_m))
   {
     LOGC_TEST_TRACE("iLogTest_Threads_g() failed");
@@ -249,7 +301,7 @@ int iLogTest_Storage_g(TagLog *ptagLog)
 #endif /* LOG_MAX_STORAGE_COUNT */
 
 #if defined(LOGC_TEST_THREADS)
-tThreadReturn LogTest_ThreadFunc_m(void *pvArgs)
+TThreadReturn LogTest_ThreadFunc_m(void *pvArgs)
 {
 #if (LOGC_TEST_THREADS==1)
   unsigned long ulThreadNr=GetCurrentThreadId();
@@ -267,7 +319,7 @@ tThreadReturn LogTest_ThreadFunc_m(void *pvArgs)
     LOGC_TEST_TEXT(ptagLog,LOGC_INFO,"Hello No.%d from Thread No.%lu",iIndex,ulThreadNr);
   }
   printf("thread %lu done\n",ulThreadNr);
-  return((tThreadReturn)0);
+  return((TThreadReturn)0);
 }
 #endif
 
@@ -323,3 +375,6 @@ int iLogTest_Threads_m(TagLog *ptagLog)
   return((uiIndex==LOGC_TEST_THREADS_COUNT)?0:-1);
 }
 #endif /* Test-Threads */
+
+
+
